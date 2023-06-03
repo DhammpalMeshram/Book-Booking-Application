232636f4bee1d62f3a66209aeeb56e9a0685a3f1
Dhammpal <dhammpal123321@gmail.com>
2023-06-03 23:02:31 +0530

    Header component added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//component import
+import Header from './Components/Header/Header';
+// import Home from './Components/Home/Home';
+// import DataProvider from './context/DataProvider';
+// import DetailsPage from './Components/DetailsPage/DetailsPage';
+// import Cart from './Components/Cart/Cart';
+// import EmptyCart from './Components/Cart/EmptyCart/EmptyCart';
+// import Payment from './Components/Payment/Payment';
+// import NotFound from './Components/NotFound/NotFound';
+// import CategoryPage from './Components/CategoryPage/CategoryPage';
+
 function App() {
+  // localStorage.setItem('cartPorducts', JSON.stringify([]));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // <DataProvider className = "App">
+        <BrowserRouter>
+          <Header/>
+          <main style={{marginTop:55}}>
+            <Routes>
+              {/* <Route path='/' element={<Home/>}/>
+              <Route path='/product/:id' element={<DetailsPage/>}/>
+              <Route path= '/cart' element= {<Cart/>}/>
+              <Route path ='/emptyCart' element={<EmptyCart/>}/>
+              <Route path ="/payment" element={<Payment/>}/>
+              <Route path = "/:serchname" element = {<CategoryPage/>}/>
+              <Route path='*' element={<NotFound />}/> */}
+            </Routes>
+          </main>
+        </BrowserRouter>
+      // </DataProvider>
   );
 }
 
--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -6,30 +6,32 @@ import {AppBar, Toolbar, styled, List, ListItem, Badge} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
 //global state imports
-import { useSelector} from 'react-redux';
-import { MyContext } from '../../context/DataProvider';
+// import { useSelector} from 'react-redux';
+// import { MyContext } from '../../context/DataProvider';
 
 //components imports
-import Login from '../Login/Login';
-import Profile from '../Profile/Profile';
+// import Login from '../Login/Login';
+// import Profile from '../Profile/Profile';
 
 //roting imports 
 import {Link, useNavigate} from 'react-router-dom';
 
 //heading styles
 const StyledHeader = styled(AppBar)`
-  background: #2874f0;
+  background: #19c33f;
   height:55px;
 `
 
 const Header =()=>{
-    const {products} = useSelector(state=> state.getProducts);
-    const {cartItems} = useSelector(state=> state.cart)
-    const myContext = useContext(MyContext);
+    // const {products} = useSelector(state=> state.getProducts);
+    // const {cartItems} = useSelector(state=> state.cart)
+    // const myContext = useContext(MyContext);
     const InputRef = useRef(null);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [text, setText] = useState('');
     const [productId, setProductId] = useState('');
@@ -41,8 +43,8 @@ const Header =()=>{
     useEffect(()=>{
         let isUserId = sessionStorage.getItem("flipcartUser");
         if(isUserId && isUserId.length >4){
-            myContext.setLoggedIn(true);
-            myContext.setUserName(isUserId);
+            // myContext.setLoggedIn(true);
+            // myContext.setUserName(isUserId);
         }
         // eslint-disable-next-line
     },[])
@@ -53,7 +55,7 @@ const Header =()=>{
     // function to display list when text is typed in input
     const onSerchClick =()=>{
         if(InputRef.current.value ==='' && productId === "") return;
-        navigate(`/product/${productId}`)
+        // navigate(`/product/${productId}`)
     }
 
     //on input change
@@ -62,25 +64,29 @@ const Header =()=>{
     //function to open the deails page when item in list is clicked
     const openDetail=(e,id)=>{
         InputRef.current.value = e.target.innerText;
-        setProductId(id)
-        navigate(`/product/${id}`);
-        setText('');
+        // setProductId(id)
+        // navigate(`/product/${id}`);
+        // setText('');
     }
 
     //function to clear input filed when home is rendered
     const clearInput = ()=>{
-        myContext.setCheckout(false);
+        // myContext.setCheckout(false);
 
         if(InputRef.current) InputRef.current.value = "";
-        setProductId('');
+        // setProductId('');
     }
 
     
     return (
         <StyledHeader style={{boxShadow: 'none'}}>
         <Toolbar  style={{minHeight:55,}}>
+                <Link to='/' className='header_1' style={{textDecoration:'none'}} >
+                    <MenuBookIcon onClick={clearInput}/>
+                    WishBook.Com
+                </Link>
             
-            <div className='header_1'>
+            {/* <div className='header_1'>
                 <Link to='/' style={{textDecoration:'none'}}>
                     <img src ={logoUrl} alt={"filpcartLogo"} onClick={clearInput}/>
                 </Link>
@@ -88,50 +94,61 @@ const Header =()=>{
                     <span>Plus</span>
                     <img width={'12px'} src={plusIconUrl} alt="icon"/>
                 </p>
-            </div>
+            </div> */}
            
-            {   !myContext.checkout && 
+            {   
+            // !myContext.checkout && 
                 <div className='header_2'>
-                    <input id={'inputField'} type={"text"} placeholder={"Search for products, brands, and more"} ref={InputRef}  onChange={onInputChanged}/>
+                    <input 
+                     id={'inputField'}
+                     type={"text"} 
+                     placeholder={"Search for books, authors or stores"} 
+                     ref={InputRef}  
+                     onChange={onInputChanged}
+                    />
                     <SearchIcon onClick={onSerchClick}/>
                 </div>
             }
-            {   text &&  
+            {/* {   text &&  
                 <List className={'listStyles'} style={{position:'absolute',top:'45px',left:"265px", color:"black", backgroundColor :"white", maxHeight:"300px", width:"478px", overflow: 'auto', fontSize:"14px", zIndex:"100"}}>
                     {
                         products.filter(product=>product.title.longTitle.toLowerCase().includes(InputRef.current.value.toLowerCase()))
                         .map(product=>(<ListItem key={product.id} onClick={(e)=>openDetail(e,product.id)} style={{cursor:'pointer'}}>{product.title.longTitle}</ListItem>))
                     }
                 </List>
-            }
-            {   !myContext.checkout &&
+            } */}
+            {/* {   !myContext.checkout && */
                 <div className={'header_3'}>
-                    { myContext.loggedin ? <Profile/>:<button onClick={onLoginCLick}>Login</button> }
+                    {/* { myContext.loggedin ? <Profile/>:<button onClick={onLoginCLick}>Login</button> } */}
+                    { <button onClick={onLoginCLick}>Login</button> }
+
                 </div>
             }
-            {   !myContext.checkout &&
+            {/* {   !myContext.checkout && */
                 <div className='header_4'>
                     <p>Become a Seller</p>
                 </div>
             }
-            {   !myContext.checkout &&
+            {/* {   !myContext.checkout && */
                 <div className='header_5'>
                     <span>More</span>
                     <ExpandMoreIcon/>
                 </div>
             }
-            {   !myContext.checkout &&
-                <div className='header_6' onClick={()=>navigate('/cart')}>
+            {/* {   !myContext.checkout && */
+                // <div className='header_6' onClick={()=>navigate('/cart')}>
+                <div className='header_6'>
+
                     <ShoppingCartIcon/>
-                    <Badge badgeContent={cartItems.length} color='secondary'>
+                    {/* <Badge badgeContent={cartItems.length} color='secondary'>
                         <span>Cart</span>
-                    </Badge>
+                    </Badge> */}
                 </div>
             }
 
         </Toolbar>
 
-        {<Login open={open} setOpen={setOpen}/>}
+        {/* {<Login open={open} setOpen={setOpen}/>} */}
       </StyledHeader>
     )
 }
