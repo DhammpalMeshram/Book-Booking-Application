1c5fc59e4eb276aa23bcef841f222282bceb7dda
Dhammpal <dhammpal123321@gmail.com>
2023-06-04 14:25:17 +0530

    cart actions created

---
+++ Client/src/Redux/actions/cartActions.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+import * as actionTypes from "../constants/cartConstants";
+const url = "http://localhost:8000";
+
+
+// middleware to get product data of id received
+export const addToCart =(id, qty)=> async(dispatch)=>{
+    try{
+        const {data} = await axios.get(`${url}/product/${id}`);
+        dispatch({type: actionTypes.ADD_TO_CART, payload:{...data, qty}});
+    }
+    catch(err){
+        dispatch({type: actionTypes.ADD_TO_CART_ERROR, payload:err.message});
+    }
+} 
+
+// middleware to remove item from cart.
+export const removeFromCart= (id)=>(dispatch)=>{
+    try{
+        dispatch({type:actionTypes.REMOVE_FROM_CART, payload:id})
+    }
+    catch(err){
+        dispatch({type:actionTypes.REMOVE_FROM_CART_FAIL, payload:err.message})
+    }
+}
+
---
+++ Client/src/Redux/constants/cartConstants.js
@@ -0,0 +1,9 @@
+
+export const ADD_TO_CART = "addToCart";
+export const ADD_TO_CART_ERROR = "addToCartError";
+export const REMOVE_FROM_CART = "removeFromCart";
+export const REMOVE_FROM_CART_FAIL = "removeFromCart";
+export const RESET_CART = "resetCart";
+export const GET_CART_DATA_FAIL = "getCartDataFail";
+export const GET_CART_DATA = "getCartData";
+export const RESET_CART_FAIL = "resetCartFail";
